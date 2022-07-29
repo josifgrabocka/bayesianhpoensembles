7b0e1daa7997f1214f790db730cd6727cc17af42
josif <josifgrabocka@gmail.com>
2022-07-29 11:20:10 +0200

    integrate openml

--- tabular/main_tabular_ensembling.py
+++ tabular/main_tabular_ensembling.py
@@ -1,32 +1,24 @@
 from BayesianHyperEnsembles import BayesianHyperEnsembles
 
 import argparse
-import os
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--task_id', help='The OpenML task id', type=int)
 parser.add_argument('--log_folder', help='Where to save the hpo logs', type=str)
 parser.add_argument('--results_folder', help='Where to save the baseline results', type=str)
+parser.add_argument('--sampler', help='random, hpo', type=str)
 args = parser.parse_args()
 
+
+# define the configuration
 config = {}
+
+config["task_id"] = args.task_id
+config["sampler"] = args.sampler
 config["log_folder"] = args.log_folder
 config["results_folder"] = args.results_folder
 
-tasks_folders = [name for name in os.listdir(config["log_folder"])
-                 if os.path.isdir(os.path.join(config["log_folder"], name)) and "task_id=" in name]
+bhe = BayesianHyperEnsembles(config=config)
+bhe.run()
 
-# aggregate the results for that task
-for task_folder in tasks_folders:
-
-    task_id = int(task_folder.split("=")[1])
-
-    config["task_id"] = task_id
-
-    for sampler in ['tpe', 'random']:
-        config["sampler"] = sampler
-
-        print('Run', config)
-
-        bhe = BayesianHyperEnsembles(config=config)
-        bhe.run()
 
