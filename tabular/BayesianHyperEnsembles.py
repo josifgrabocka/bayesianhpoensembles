bc39bc135ca7d87157361d0b0d027d340a6c188e
josifgrabocka <33453152+josifgrabocka@users.noreply.github.com>
2022-07-31 20:19:45 +0200

    fix likelihoods

--- tabular/BayesianHyperEnsembles.py
+++ tabular/BayesianHyperEnsembles.py
@@ -107,8 +107,11 @@ class BayesianHyperEnsembles:
 
         elif posterior_type == "bayesian-rank":
             accuracies_series = pd.Series(val_likelihoods)
-            accuracies_ranks_exp = np.exp(self.alpha*accuracies_series.rank().to_numpy())
-            posteriors = accuracies_ranks_exp / np.sum(accuracies_ranks_exp)
+            #accuracies_ranks_exp = np.exp(self.alpha*accuracies_series.rank().to_numpy())
+            #posteriors = accuracies_ranks_exp / np.sum(accuracies_ranks_exp)
+
+            accuracies_ranks = accuracies_series.rank().to_numpy()
+            posteriors = accuracies_ranks / np.sum(accuracies_ranks)
 
         elif posterior_type == "uniform":
             posteriors = np.ones_like(val_likelihoods) / float(len(val_likelihoods))
